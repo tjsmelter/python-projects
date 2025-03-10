6e6af14e03bfca9e5230b4e9be1446d52e24250e
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-10 14:31:26 -0700

    Create scoreboard.py

---
+++ 14. Frogger Game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.goto(-190,250)
+        self.write(f"Level: {self.level}",align="center", font=FONT)
+
+    def increase_level(self):
+        self.clear()
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER",False,align="center",font=FONT)
