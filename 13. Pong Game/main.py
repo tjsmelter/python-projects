b14f904009a5da384a2a95f80cbad9068a9064b2
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-10 14:32:11 -0700

    Update main.py

--- 13. Pong Game/main.py
+++ 13. Pong Game/main.py
@@ -1,3 +1,5 @@
+# The following code does not reflect original work, rather the result of learning via the course "100 Days of Code: The Complete Python Pro Bootcamp"
+
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
