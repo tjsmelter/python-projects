0634917be7e616f726158820a52d8b9a26b65f8d
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-10 11:07:23 -0700

    Update food.py

--- 12. Snake Game/food.py
+++ 12. Snake Game/food.py
@@ -3,7 +3,9 @@
 from turtle import Turtle
 import random
 
+
 class Food(Turtle):
+    # initialize a class for the "food" object, size, color, and random position on the screen
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -16,7 +18,8 @@ class Food(Turtle):
         self.penup()
         self.goto(random_x_cor, random_y_cor)
         self.generate_food()
-    
+
+    # create a function that will move the food object to a new random place on the screen
     def generate_food(self):
         new_food_x_cor = random.randint(-250, 250)
         new_food_y_cor = random.randint(-250, 250)
