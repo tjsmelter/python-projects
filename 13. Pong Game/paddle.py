7628940e785c4017dde056e57d72caef5a4376f9
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-10 14:28:06 -0700

    Create paddle.py

---
+++ 13. Pong Game/paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+    # initialize the paddle object for shape, color and to go to a given position as indicated in an argument
+    def __init__(self,position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    # create a function that will gather the current y coordinate and move the paddle "north" 20 units 
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    # create a function that will gather the current y coordinate and move the paddle "south" 20 units 
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
