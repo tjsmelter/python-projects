05b6f39d803948fe42ffb90c53e737d9cb8b3cda
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-10 11:13:52 -0700

    Update scoreboard.py

--- 12. Snake Game/scoreboard.py
+++ 12. Snake Game/scoreboard.py
@@ -5,24 +5,27 @@ ALIGNMENT = "center"
 FONT = ("Futura", 24, "normal")
 
 class Scoreboard(Turtle):
+    # initialize the scoreboard class that will set an attribute score to start at 0, the color, and position on the screen
     def __init__(self):
         super().__init__()
         self.score = 0
         self.color("white")
         self.penup()
-        # self.shapesize()
-        # self.speed("fastest")
         self.goto(0,270)
         self.hideturtle()
 
+    # create a function that will display the current score on screen
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
 
+    # create a function that will update the score based on the snake eating the food, 
+    # clear the screen of the previous score, and update the scoreboard to show the new score
     def accumulate_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
+    # create a function that will display "GAME OVER" in the center of the screen
     def game_over(self):
         self.goto(0,0)
         self.write("GAME OVER", False,ALIGNMENT, FONT)
