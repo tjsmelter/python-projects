7f76e66c53778857d347792743013eef5682e30c
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-10 14:33:07 -0700

    Update main.py

--- 14. Frogger Game/main.py
+++ 14. Frogger Game/main.py
@@ -1,4 +1,4 @@
-
+# The following code does not reflect original work, rather the result of learning via the course "100 Days of Code: The Complete Python Pro Bootcamp"
 
 import time
 from turtle import Screen
