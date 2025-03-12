253abb8e713f5e0a6eaa65247eca29a2afcc35a0
Thomas Smelter <103446589+tjsmelter@users.noreply.github.com>
2025-03-12 12:58:53 -0700

    Update main.py

--- 15. Mail Merge/main.py
+++ 15. Mail Merge/main.py
@@ -1,3 +1,5 @@
+# The following code does not reflect original work, rather the result of learning via the course "100 Days of Code: The Complete Python Pro Bootcamp"
+
 PLACEHOLDER = "[name]"
 
 with open("./Input/Names/invited_names.txt","r") as names:
